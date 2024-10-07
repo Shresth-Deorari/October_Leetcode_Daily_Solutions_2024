7ce3fb16dec6e2df753621ad5d5728e1ece86ac3
Shresth-Deorari <shresthdeorari38@gmail.com>
2024-10-07 09:42:52 +0530

    Add files via upload

---
+++ October_7.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(auto i : s){
+            if(!st.empty() && ((st.top()=='A' && i =='B' ) || (st.top()=='C' && i =='D'))){
+                st.pop();
+            }
+            else{
+                st.push(i);
+            }
+        }
+        return st.size();
+    }
+};
